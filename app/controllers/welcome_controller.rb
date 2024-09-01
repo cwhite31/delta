3a85e5421e40cb763e61345146061653505b7e67
Chris White <cwhite31@gmail.com>
2024-09-01 17:38:46 +0800

    devise

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   before_action :authenticate_user!
 
-  
+
   def index
   @last = Contact.last
   @monthly = Contact.this_year.group_by_month(:qso_date, format: "%b %Y").count
@@ -9,7 +9,7 @@ class WelcomeController < ApplicationController
   @bands = Contact.select(:band).distinct
 
 
-  sql_source = "'select country, band, count(band) from contacts c group by country, band order by 1,2 desc'"
+  sql_source = "'select country, band, count(band) from contacts c where band ! = '17m' group by country, band order by 1,2 desc'"
   sql_cat = "'select distinct band from contacts dxcc order by 1'"
   # sql = "select * from crosstab(" + sql_source + ", " + sql_cat + ') AS ct(country text, "10m" int, "12m" int, "15m" int, "20m" int, "40m" int, "6m" int);'
   #add 17m
