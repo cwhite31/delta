b3ba39a37ac7f7ad343dd6346913d0bc8e420887
Chris White <cwhite31@gmail.com>
2025-02-10 10:17:22 +0800

    states

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_24_132843) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_09_070642) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "tablefunc"
@@ -133,6 +133,11 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_24_132843) do
     t.string "category"
     t.string "title", null: false
     t.text "note"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reports", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
